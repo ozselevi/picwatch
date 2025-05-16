9adfbc8992649d9546a9a2ad2cd2f509cd8fd633
vagrant <vagrant@CloudNativeGo>
2025-05-16 18:51:10 +0000

    jav

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -2,11 +2,17 @@ from celery import Celery
 
 celery_app = Celery(
     "worker",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker="redis://redis:6379/0",    # Kubernetesben, ha localhostban akkor 'redis://localhost:6379/0'
+    backend="redis://redis:6379/0"
 )
 
 @celery_app.task
 def test_task(message):
     print(f"Celery task received message: {message}")
     return f"Processed message: {message}"
+
+
+
+
+
+
