7bfd1f2525bcf6ad5d7fad1fdc36f4505ea2699f
vagrant <vagrant@CloudNativeGo>
2025-05-17 06:55:44 +0000

    jav

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 from database import Base, engine, SessionLocal
+from models import Image as ImageModel
 
 celery_app = Celery(
     "worker",
